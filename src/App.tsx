ac5086b4330ff964607964a37ec83c8a792e8a05
Catolica <Catolica@C073010.pucp.edu.pe>
2024-06-08 20:59:41 -0500

    Added scanner

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, Component } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 
-const client = generateClient<Schema>();
+import QrReader from 'react-qr-scanner'
+
+//const client = generateClient<Schema>();
+
+function Test() {
+
+  const [result, setResult] = useState('')
+
+  function handleScan(data) {
+
+    if (data != null) {
+      setResult(data.text)
+    }
+
+  }
+
+  function handleError(err) {
+    console.log(err)
+  }
+
+  const previewStyle = {
+    height: 240,
+    width: 320
+  }
+
+  return <div>
+    <QrReader
+      delay="100"
+      style={previewStyle}
+      onError={handleError}
+      onScan={handleScan}
+    />
+  <p>{result}</p>
+  </div>
+}
 
 function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
+  //const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+
+  /*
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
@@ -17,21 +53,26 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+  <button onClick={createTodo}>+ new</button>
+  <ul>
+    {todos.map((todo) => (
+      <li key={todo.id}>{todo.content}</li>
+    ))}
+  </ul>
+  
+  */
+
   return (
     <main>
       <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
         <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
         </a>
+        <Test/>
       </div>
     </main>
   );
